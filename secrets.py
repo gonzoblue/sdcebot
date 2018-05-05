f3f95fb006f2af9c1f18ec9111323d2b957b179b
Alex <gonzoblue@users.noreply.github.com>
2018-05-05 12:55:29 -0700

    Update secrets.py
    Added support for multiple TO addresses

--- secrets.py
+++ secrets.py
@@ -1,5 +1,5 @@
 EMAIL_FROM = 'from@domain.com'
-EMAIL_TO = 'to@gmail.com'
+EMAIL_TO = ["person1@email.com", "person2@email.com"]
 EMAIL_PASS = 'password'
 EMAIL_SERVER = 'mail.domain.com'
 DBPATH = "/home/user/sdcebot/"
